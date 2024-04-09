0baffb8bf4c9439ed0b01836893bcf266f6d2d1f
Daniel Beranek <daniel.beraanek@gmail.com>
2024-04-09 20:15:14 +0200

    Add WindLoadGenerator

---
+++ src/desssign/loads/wind/roofs.py
@@ -0,0 +1,306 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from desssign.loads.wind.external_pressure_coefficients import get_flat_roof_coefficient
+from desssign.loads.wind.external_pressure_coefficients import get_duopitch_roof_coefficient
+from desssign.loads.wind.wind_load import WindLoad
+
+if TYPE_CHECKING:
+    from desssign.loads.wind.enums import WindZone
+    from desssign.loads.wind.enums import TerrainCategory
+    from desssign.loads.wind.enums import FlatRoofType
+
+
+class Roof:
+    """
+    Abstract class for roofs.
+
+    :ivar zone: Wind zone.
+    :ivar terrain_category: Terrain category.
+    :ivar b_x: Width of the building in the x-direction.
+    :ivar b_y: Width of the building in the y-direction.
+    """
+    def __init__(
+        self,
+        zone: str | WindZone,
+        terrain_category: str | TerrainCategory,
+        b_x: float,
+        b_y: float,
+    ) -> None:
+        self.zone = zone
+        self.terrain_category = terrain_category
+        self.b_x = b_x
+        self.b_y = b_y
+
+
+class FlatRoof(Roof):
+    """
+    Class for flat roofs.
+
+    :ivar roof_type: Type of the flat roof.
+    :ivar zone: Wind zone.
+    :ivar terrain_category: Terrain category.
+    :ivar b_x: Width of the building in the x-direction.
+    :ivar b_y: Width of the building in the y-direction.
+    :ivar h: Height of the building.
+    :ivar h_p: Height of the parapet.
+    :ivar wind_load: Instance of the :class:`WindLoad` class.
+    """
+    def __init__(
+        self,
+        roof_type: str | FlatRoofType,
+        zone: str | WindZone,
+        terrain_category: str | TerrainCategory,
+        b_x: float,
+        b_y: float,
+        h: float,
+        h_p: float = 0.0,
+    ) -> None:
+        super().__init__(zone, terrain_category, b_x, b_y)
+        self.roof_type = roof_type
+        self.h = h
+        self.h_p = h_p
+        self.wind_load = WindLoad(self.zone, self.terrain_category, self.z_e)
+
+    @property
+    def z_e(self) -> float:
+        """Reference height."""
+        return self.h + self.h_p
+
+    def calculate_zones(self, b: float, d: float, sign: str, direction: str) -> list[RoofZone]:
+        e = min(b, 2 * self.h)
+        ratio = self.h_p / self.h if self.h_p != 0.0 else None
+        c_pe = {}
+        for zone_label in ["F", "G", "H", "I"]:
+            c_pe[zone_label] = get_flat_roof_coefficient(
+                self.roof_type, zone_label, "c_pe10", sign=sign, ratio=ratio
+            )
+
+        if direction.lower() == "x":
+            zones = [
+                RoofZone("F", 0.0, 0.0, e / 10, e / 4, c_pe["F"], c_pe["F"] * self.wind_load.q_p),
+                RoofZone("G", 0.0, e / 4, e / 10, b - 2 * e / 4, c_pe["G"], c_pe["G"] * self.wind_load.q_p),
+                RoofZone("F", 0.0, b - e / 4, e / 10, e / 4, c_pe["F"], c_pe["F"] * self.wind_load.q_p),
+            ]
+            if d - e / 2 > 0:
+                zones.append(RoofZone("H", e / 10, 0.0, e / 2 - e / 10, b, c_pe["H"], c_pe["H"] * self.wind_load.q_p))
+                zones.append(RoofZone("I", e / 2, 0.0, d - e / 2, b, c_pe["I"], c_pe["I"] * self.wind_load.q_p))
+            else:
+                zones.append(RoofZone("H", e / 10, 0.0, d - e / 10, b, c_pe["H"], c_pe["H"] * self.wind_load.q_p))
+
+        elif direction.lower() == "y":
+            zones = [
+                RoofZone("F", 0.0, 0.0, e / 4, e / 10, c_pe["F"], c_pe["F"] * self.wind_load.q_p),
+                RoofZone("G", e / 4, 0.0, b - 2 * e / 4, e / 10, c_pe["G"], c_pe["G"] * self.wind_load.q_p),
+                RoofZone("F", b - e / 4, 0.0, e / 4, e / 10, c_pe["F"], c_pe["F"] * self.wind_load.q_p),
+            ]
+
+            if d - e / 2 > 0:
+                zones.append(RoofZone("H", 0.0, e / 10, b, e / 2 - e / 10, c_pe["H"], c_pe["H"] * self.wind_load.q_p))
+                zones.append(RoofZone("I", 0.0, e / 2, b, d - e / 2, c_pe["I"], c_pe["I"] * self.wind_load.q_p))
+            else:
+                zones.append(RoofZone("H", 0.0, e / 10, b, d - e / 10, c_pe["H"], c_pe["H"] * self.wind_load.q_p))
+
+        else:
+            raise ValueError(f"Direction must be either 'x' or 'y', not: '{direction}'.")
+        return zones
+
+    @property
+    def zones_wind_x_pressure(self) -> list[RoofZone]:
+        return self.calculate_zones(self.b_y, self.b_x, "+", "x")
+
+    @property
+    def zones_wind_x_suction(self) -> list[RoofZone]:
+        return self.calculate_zones(self.b_y, self.b_x, "-", "x")
+
+    @property
+    def zones_wind_y_pressure(self) -> list[RoofZone]:
+        return self.calculate_zones(self.b_x, self.b_y, "+", "y")
+
+    @property
+    def zones_wind_y_suction(self) -> list[RoofZone]:
+        return self.calculate_zones(self.b_x, self.b_y, "-", "y")
+
+
+class MonopitchRoof(Roof):
+    """
+    Class for monopitched roofs.
+
+    :ivar zone: Wind zone.
+    :ivar terrain_category: Terrain category.
+    :ivar b_x: Width of the building in the x-direction.
+    :ivar b_y: Width of the building in the y-direction.
+    :ivar h: Height of the building.
+    :ivar wind_load: Instance of the :class:`WindLoad` class.
+    """
+    def __init__(
+        self,
+        zone: str | WindZone,
+        terrain_category: str | TerrainCategory,
+        b_x: float,
+        b_y: float,
+        h: float
+    ) -> None:
+        super().__init__(zone, terrain_category, b_x, b_y)
+        self.h = h
+        self.wind_load = WindLoad(self.zone, self.terrain_category, self.z_e)
+        raise NotImplementedError("MonopitchedRoof class is not implemented yet.")
+
+    @property
+    def z_e(self) -> float:
+        """Reference height."""
+        return self.h
+
+
+class DuopitchRoof(Roof):
+    def __init__(
+        self,
+        zone: str | WindZone,
+        terrain_category: str | TerrainCategory,
+        b_x: float,
+        b_y: float,
+        h: float,
+        pitch_angle: float
+    ) -> None:
+        super().__init__(zone, terrain_category, b_x, b_y)
+        self.h = h
+        self.pitch_angle = pitch_angle
+        self.wind_load = WindLoad(self.zone, self.terrain_category, self.z_e)
+
+    @property
+    def z_e(self) -> float:
+        """Reference height."""
+        return self.h
+
+    def calculate_zones(
+        self,
+        b: float,
+        d: float,
+        signs: list[str],
+        wind_direction: str,
+    ) -> list[RoofZone]:
+        e = min(b, 2 * self.h)
+
+        c_pe = {}
+
+        zone_keys = ["F", "G", "H", "I"]
+        zone_signs = [signs[0]] * 3 + [signs[1]] * 1
+        if wind_direction == "x":
+            zone_keys.append("J")
+            zone_signs.append(signs[1])
+
+        for zone_key, sign in zip(zone_keys, zone_signs):
+            c_pe[zone_key] = get_duopitch_roof_coefficient(
+                zone_key, "c_pe10", sign, self.pitch_angle, wind_direction
+            )
+
+        if wind_direction.lower() == "x":
+            zones = [
+                RoofZone("F", 0.0, 0.0, e / 10, e / 4, c_pe["F"], c_pe["F"] * self.wind_load.q_p),
+                RoofZone("G", 0.0, e / 4, e / 10, b - 2 * e / 4, c_pe["G"], c_pe["G"] * self.wind_load.q_p),
+                RoofZone("F", 0.0, b - e / 4, e / 10, e / 4, c_pe["F"], c_pe["F"] * self.wind_load.q_p),
+                RoofZone("H", e / 10, 0.0, d / 2 - e / 10, b, c_pe["H"], c_pe["H"] * self.wind_load.q_p),
+                RoofZone("J", d / 2, 0.0, e / 10, b, c_pe["J"], c_pe["J"] * self.wind_load.q_p),
+                RoofZone("I", d / 2 + e / 10, 0.0, d / 2 - e / 10, b, c_pe["I"], c_pe["I"] * self.wind_load.q_p),
+            ]
+
+        elif wind_direction.lower() == "y":
+            zones = [
+                RoofZone("F", 0.0, 0.0, e / 4, e / 10, c_pe["F"], c_pe["F"] * self.wind_load.q_p),
+                RoofZone("G", e / 4, 0.0, b / 2 - e / 4, e / 10, c_pe["G"], c_pe["G"] * self.wind_load.q_p),
+                RoofZone("G", b / 2, 0.0, b / 2 - e / 4, e / 10, c_pe["G"], c_pe["G"] * self.wind_load.q_p),
+                RoofZone("F", b - e / 4, 0.0, e / 4, e / 10, c_pe["F"], c_pe["F"] * self.wind_load.q_p),
+                RoofZone("H", 0.0, e / 10, b / 2, e / 2 - e / 10, c_pe["H"], c_pe["H"] * self.wind_load.q_p),
+                RoofZone("H", b / 2, e / 10, b / 2, e / 2 - e / 10, c_pe["H"], c_pe["H"] * self.wind_load.q_p),
+                RoofZone("I", 0.0, e / 2, b / 2, d - e / 2, c_pe["I"], c_pe["I"] * self.wind_load.q_p),
+                RoofZone("I", b / 2, e / 2, b / 2, d - e / 2, c_pe["I"], c_pe["I"] * self.wind_load.q_p),
+            ]
+
+        else:
+            raise ValueError(f"Direction must be either 'x' or 'y', not: '{wind_direction}'.")
+
+        return zones
+
+    @property
+    def zones_wind_x_neg_neg(self) -> list[RoofZone] | None:
+        if 45.0 < self.pitch_angle <= 75.0:
+            return None
+
+        return self.calculate_zones(self.b_y, self.b_x, ["-", "-"], "x")
+
+    @property
+    def zones_wind_x_neg_pos(self) -> list[RoofZone] | None:
+        if not (-5.0 <= self.pitch_angle <= 45.0):
+            return None
+
+        return self.calculate_zones(self.b_y, self.b_x, ["-", "+"], "x")
+
+    @property
+    def zones_wind_x_pos_neg(self) -> list[RoofZone] | None:
+        if not (5.0 <= self.pitch_angle <= 75.0):
+            return None
+
+        return self.calculate_zones(self.b_y, self.b_x, ["+", "-"], "x")
+
+    @property
+    def zones_wind_x_pos_pos(self) -> list[RoofZone] | None:
+        if not (5.0 <= self.pitch_angle <= 45.0):
+            return None
+
+        return self.calculate_zones(self.b_y, self.b_x, ["+", "+"], "x")
+
+    @property
+    def zones_wind_y(self) -> list[RoofZone] | None:
+        return self.calculate_zones(self.b_x, self.b_y, ["-", "-"], "y")
+
+
+class RoofZone:
+    def __init__(
+        self,
+        roof_zone: str,
+        x_bl: float,
+        y_bl: float,
+        b_x: float,
+        b_y: float,
+        c_pe: float,
+        w_e: float,
+    ) -> None:
+        self.roof_zone = roof_zone
+        self.c_pe = c_pe
+        self.w_e = w_e
+
+        self.x_bl = x_bl
+        self.y_bl = y_bl
+        self.b_x = b_x
+        self.b_y = b_y
+
+        self.geometry = calculate_rectangle_points(x_bl, y_bl, b_x, b_y)
+
+    def __str__(self) -> str:
+        return f"RoofZone({self.roof_zone}, {self.geometry}, {self.c_pe}, {self.w_e})"
+
+
+def calculate_rectangle_points(x_bl: float, y_bl: float, dx: float, dy: float) -> list[list[float]]:
+    """
+    Calculate corner points of a rectangle.
+
+    :param x_bl: X coordinate of the bottom-left corner.
+    :param y_bl: Y coordinate of the bottom-left corner.
+    :param dx: Width of the rectangle.
+    :param dy: Height of the rectangle.
+    :return: A list of corner points of the rectangle.
+    """
+    return [
+        [x_bl, y_bl],
+        [x_bl + dx, y_bl],
+        [x_bl + dx, y_bl + dy],
+        [x_bl, y_bl + dy]
+    ]
+
+
+if __name__ == "__main__":
+    roof = DuopitchRoof("I", "II", 10.0, 20.0, 30.0, 30.0)
+
+    print("")
+
---
+++ src/desssign/loads/wind/wind_load.py
@@ -0,0 +1,190 @@
+from __future__ import annotations
+
+from math import log
+
+from desssign.loads.wind.enums import WindZone
+from desssign.loads.wind.enums import TerrainCategory
+
+from desssign.loads.wind.constants import FUNDAMENTAL_VALUE_OF_BASIC_WIND_VELOCITY
+from desssign.loads.wind.constants import ROUGHNESS_LENGTH
+from desssign.loads.wind.constants import MINIMUM_HEIGHT
+from desssign.loads.wind.constants import DIRECTIONAL_FACTOR
+from desssign.loads.wind.constants import SEASONAL_FACTOR
+from desssign.loads.wind.constants import AIR_DENSITY
+
+
+class WindLoad:
+    """
+    :ivar zone: Wind zone.
+    :ivar c_dir: Directional factor.
+    :ivar c_season: Seasonal factor.
+    :ivar rho_air: Density of air in kg/m³.
+    """
+    def __init__(self,
+                 zone: str | WindZone,
+                 terrain_category: str | TerrainCategory,
+                 z_e: float,
+                 area: float = 10.0,
+                 c_dir: float = DIRECTIONAL_FACTOR,
+                 c_season: float = SEASONAL_FACTOR,
+                 rho_air: float = AIR_DENSITY
+                 ):
+        self.zone = zone
+        self.terrain_category = terrain_category
+        self.z_e = z_e
+        self.area = area
+
+        self.c_dir = c_dir
+        self.c_season = c_season
+        self.rho_air = rho_air
+
+    @property
+    def v_b0(self) -> float:
+        """
+        Fundamental value of basic wind velocity in m/s according to EN 1991-1-4.
+
+        :return: Fundamental value of basic wind velocity in m/s.
+        """
+        return FUNDAMENTAL_VALUE_OF_BASIC_WIND_VELOCITY[self.zone]
+
+    @property
+    def v_b(self) -> float:
+        """
+        Basic wind velocity in m/s according to EN 1991-1-4, eq. (4.1).
+
+        The basic wind velocity is defined as function of wind direction and time of year
+        at 10 m above the ground of terrain category II.
+
+        :return: Basic wind velocity in m/s.
+        """
+        return self.c_dir * self.c_season * self.v_b0
+
+    @property
+    def q_b(self) -> float:
+        """
+        Basic velocity pressure in N/m² according to EN 1991-1-4, eq. (4.10).
+
+        :return: Basic velocity pressure in N/m².
+        """
+        return 0.5 * self.rho_air * self.v_b ** 2
+
+    @property
+    def z_0(self) -> float:
+        """
+        Roughness length in m according to EN 1991-1-4, table 4.1.
+        """
+        return ROUGHNESS_LENGTH[self.terrain_category]
+
+    @property
+    def z_min(self) -> float:
+        """
+        Minimum height in m according to EN 1991-1-4, table 4.1.
+        """
+        return MINIMUM_HEIGHT[self.terrain_category]
+
+    @property
+    def z_max(self) -> float:
+        """
+        Maximum height in m according to EN 1991-1-4.
+        """
+        return 200.0
+
+    @property
+    def k_r(self) -> float:
+        """
+        Terrain factor.
+
+        According to EN 1991-1-4, 4.3.2, eq. (4.5).
+        """
+        z_0II: float = ROUGHNESS_LENGTH[TerrainCategory.II]
+
+        return 0.19 * (self.z_0 / z_0II) ** 0.07  # type: ignore[no-any-return]
+
+    @property
+    def k_l(self) -> float:
+        """
+        Turbulence factor.
+
+        According to EN 1991-1-4, 4.4.1(1), note 2.
+        """
+        return 1.0
+
+    @property
+    def sigma_v(self) -> float:
+        """
+        Standard deviation of the turbulence in m/s according to EN 1991-1-4, 4.4(1), eq. (4.6).
+        """
+        return self.k_r * self.v_b * self.k_l
+
+    @property
+    def c_o(self) -> float:
+        """
+        Orography factor.
+        """
+        return 1.0
+
+    @property
+    def c_r(self) -> float:
+        """
+        Roughness factor according to EN 1991-1-4, 4.3.2(1), eq. (4.4).
+        """
+        if self.z_min <= self.z_e <= self.z_max:
+            return self.k_r * log(self.z_e / self.z_0)
+        elif self.z_e < self.z_min:
+            return self.k_r * log(self.z_min / self.z_0)
+        else:
+            raise ValueError("Height above the maximum height is not supported.")
+
+    @property
+    def v_m(self) -> float:
+        """
+        Mean wind velocity in m/s according to EN 1991-1-4, 4.3.1 (1), eq. (4.3).
+        """
+        return self.c_r * self.c_o * self.v_b
+
+    @property
+    def I_v(self) -> float:
+        """
+        Turbulence intensity according to EN 1991-1-4, 4.4.1(1), eq. (4.7).
+        """
+        if self.z_min <= self.z_e <= self.z_max:
+            return self.sigma_v / self.v_m
+        elif self.z_e < self.z_min:
+            return self.sigma_v / self.v_m
+        else:
+            raise ValueError("Height above the maximum height is not supported.")
+
+    @property
+    def q_p(self) -> float:
+        """
+        Peak velocity pressure in N/m² according to EN 1991-1-4, 4.5.1(1), eq. (4.8).
+        """
+        return (1 + 7 * self.I_v) * 1/2 * self.rho_air * self.v_m ** 2
+
+    @property
+    def c_e(self) -> float:
+        """
+        Exposure factor according to EN 1991-1-4, 4.5.1(1), eq. (4.9).
+        """
+        return self.q_p / self.q_b
+
+
+if __name__ == "__main__":
+    wind = WindLoad(zone="II", terrain_category="III", z_e=7.72)
+    print(wind.v_b0)
+    print(wind.v_b)
+    print(wind.q_b)
+    print(wind.z_0)
+    print(wind.z_min)
+    print(wind.z_max)
+    print(wind.k_r)
+    print(wind.k_l)
+    print(wind.sigma_v)
+    print(wind.c_o)
+    print(wind.c_r)
+    print(wind.v_m)
+    print(wind.I_v)
+    print(wind.q_p)
+    print(wind.c_e)
+
+    print("")
